db7ece6e4021a5db5efffe22ebc466e5b30d80cf
Jennifer Hurd <jenhurd8@gmail.com>
2018-06-08 15:02:08 -0500

    First commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const tControl = require("./controllers/trivia_controller");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static("./build"));
+
+let triviaArray = [];
+let id = 0;
+
+axios.get("http://jservice.io/api/random?count=100").then(function(response) {
+  triviaArray = response.data;
+  console.log(triviaArray);
+});
+
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
